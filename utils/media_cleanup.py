3e7cec1e74150b42e7fc38feab61e866d02ecc46
medineearyal <152002363+medineearyal@users.noreply.github.com>
2025-08-04 09:05:38 +0545

    Changes
    Change

--- utils/media_cleanup.py
+++ utils/media_cleanup.py
@@ -11,7 +11,7 @@ def delete_old_file_on_update(instance, model,field_name):
     if old_file and old_file != new_file:
         if os.path.isfile(old_file.path):
             os.remove(old_file.path)
-    def delete_file_on_delete(instance, field_name):
-        file_field = getattr(instance,field_name)
-        if file_field and os.path.isfile(file_field.path):
-            os.remove(file_field.path)
+def delete_file_on_delete(instance, field_name):
+    file_field = getattr(instance,field_name)
+    if file_field and os.path.isfile(file_field.path):
+        os.remove(file_field.path)
